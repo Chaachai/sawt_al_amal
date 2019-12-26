b0f399a136b6392e0139c706d0fffcf024c6877b
ilham <ilhamazalmad11@gmail.com>
2019-12-26 02:16:07 +0100

    Created lessons interface

--- app/src/main/java/com/example/sawt_al_amal/HomeActivity.java
+++ app/src/main/java/com/example/sawt_al_amal/HomeActivity.java
@@ -1,14 +1,30 @@
 package com.example.sawt_al_amal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    CardView lessons_cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        lessons_cv = findViewById(R.id.lessons_cv);
+
+        lessons_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, LessonsActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sawt_al_amal/login.java
+++ app/src/main/java/com/example/sawt_al_amal/login.java
@@ -2,13 +2,29 @@ package com.example.sawt_al_amal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.nio.file.Files;
 
 public class login extends AppCompatActivity {
+
+    Button btlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btlogin = findViewById(R.id.btlogin);
+
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, HomeActivity.class));
+            }
+        });
     }
 }
